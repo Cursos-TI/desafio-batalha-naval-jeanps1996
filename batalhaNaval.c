6d306ff2225df44e50026b091b47ddd8a0e69cc5
jeanps1996 <jeanpachecosoares@gmail.com>
2025-04-22 15:12:56 -0300

    Update batalhaNaval.c
    implementando um navio na diagonal no tabuleiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,43 +1,66 @@
 #include <stdio.h>
 
-int main() {
-    int tabuleiro[10][10];
-    int i, j;
+#define TAM 10
+#define NAVIO 3
+#define AGUA 0
 
-    // Inicializando o tabuleiro com 0 (água)
-    for(i = 0; i < 10; i++) {
-        for(j = 0; j < 10; j++) {
-            tabuleiro[i][j] = 0;
+// Função para verificar se é possível posicionar o navio sem ultrapassar os limites e sem sobreposição
+int pode_posicionar(int tabuleiro[TAM][TAM], int linha, int coluna, int tamanho, char direcao) {
+    for (int i = 0; i < tamanho; i++) {
+        int l = linha;
+        int c = coluna;
+
+        if (direcao == 'H') c += i;              // Horizontal
+        else if (direcao == 'V') l += i;         // Vertical
+        else if (direcao == 'D') { l += i; c += i; }     // Diagonal principal
+        else if (direcao == 'O') { l += i; c -= i; }     // Diagonal oposta
+
+        if (l >= TAM || c >= TAM || c < 0 || tabuleiro[l][c] != AGUA) {
+            return 0;  // Fora do tabuleiro ou sobreposição
         }
     }
+    return 1;
+}
 
-    // Declarando navios com tamanho 3
-    int navio_horizontal[3] = {3, 3, 3};
-    int navio_vertical[3] = {3, 3, 3};
-
-    // Coordenadas iniciais dos navios
-    int linha_horizontal = 2, coluna_horizontal = 2;  // posição inicial do navio horizontal
-    int linha_vertical = 5, coluna_vertical = 7;      // posição inicial do navio vertical
-
-    // Posicionando navio horizontal no tabuleiro
-    for(i = 0; i < 3; i++) {
-        tabuleiro[linha_horizontal][coluna_horizontal + i] = navio_horizontal[i];
+// Função para posicionar o navio após validar
+void posicionar_navio(int tabuleiro[TAM][TAM], int linha, int coluna, int tamanho, char direcao) {
+    for (int i = 0; i < tamanho; i++) {
+        if (direcao == 'H') tabuleiro[linha][coluna + i] = NAVIO;
+        else if (direcao == 'V') tabuleiro[linha + i][coluna] = NAVIO;
+        else if (direcao == 'D') tabuleiro[linha + i][coluna + i] = NAVIO;
+        else if (direcao == 'O') tabuleiro[linha + i][coluna - i] = NAVIO;
     }
+}
 
-    // Posicionando navio vertical no tabuleiro
-    for(i = 0; i < 3; i++) {
-        tabuleiro[linha_vertical + i][coluna_vertical] = navio_vertical[i];
-    }
-
-    // Imprimindo o tabuleiro
-    printf("Tabuleiro Batalha Naval:\n\n");
-    for(i = 0; i < 10; i++) {
-        for(j = 0; j < 10; j++) {
+// Função para exibir o tabuleiro
+void exibir_tabuleiro(int tabuleiro[TAM][TAM]) {
+    printf("Tabuleiro Batalha Naval 10x10:\n");
+    for (int i = 0; i < TAM; i++) {
+        for (int j = 0; j < TAM; j++) {
             printf("%d ", tabuleiro[i][j]);
         }
         printf("\n");
     }
+}
+
+int main() {
+    int tabuleiro[TAM][TAM] = {0};
+
+    // Tentamos posicionar navios com tamanhos diferentes
+    if (pode_posicionar(tabuleiro, 2, 1, 4, 'H'))  // Navio horizontal
+        posicionar_navio(tabuleiro, 2, 1, 4, 'H');
+
+    if (pode_posicionar(tabuleiro, 5, 5, 3, 'V'))  // Navio vertical
+        posicionar_navio(tabuleiro, 5, 5, 3, 'V');
+
+    if (pode_posicionar(tabuleiro, 0, 0, 4, 'D'))  // Diagonal principal
+        posicionar_navio(tabuleiro, 0, 0, 4, 'D');
+
+    if (pode_posicionar(tabuleiro, 0, 9, 4, 'O'))  // Diagonal oposta
+        posicionar_navio(tabuleiro, 0, 9, 4, 'O');
+
+    // Exibir o resultado
+    exibir_tabuleiro(tabuleiro);
 
     return 0;
 }
-
